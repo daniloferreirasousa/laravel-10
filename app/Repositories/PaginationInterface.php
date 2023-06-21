80214213cf32f157d0722a4ed13c8b7a7dfb1292
Andrade <andradesilva.danilo@eb.mil.br>
2023-06-21 16:49:58 -0300

    add interface Pagination

--- app/Repositories/PaginationInterface.php
+++ app/Repositories/PaginationInterface.php
@@ -4,5 +4,16 @@ namespace App\Repositories;
 
 interface PaginationInterface
 {
-    
+    /**
+     * Undocumented function
+     *
+     * @return stdClass[]
+     */
+    public function item(): array;
+    public function total():int;
+    public function isFirstPage(): bool;
+    public function isLastPage(): bool;
+    public function currentPage(): int;
+    public function getNumberNextPage(): int;
+    public function getNumberPreviusPage(): int;
 }
